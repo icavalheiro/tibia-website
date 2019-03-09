908c3217128e2bf1d3d5841b83f702c37f4a68b9
Ivan S. Cavalheiro <ivanscavalheiro@hotmail.com>
2019-03-09 20:58:00 -0300

    Removed CDN references from the code, now we will use jquery and bootstrap from npm

--- src/TibiaWebsite/Js/site.js
+++ src/TibiaWebsite/Js/site.js
@@ -1,3 +1,9 @@
+import jQuery from "jquery";
+//setup jquery globally
+window.jQuery = window.$ = jQuery;
+
+import "popper.js";
+import "bootstrap";
 import ButtonHelper from "./helpers/button";
 
 //wait for the page to finish loading
